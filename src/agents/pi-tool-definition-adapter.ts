daverjorge46/yoyoclaw

feat: implement before_tool_result hook (Phases 1-3)
- Adds before_tool_result hook type definitions
- Implements runBeforeToolResultHook() helper
- Integrates hook into tool execution flow (success + error paths)
- Adds toClientToolDefinitions() for OpenResponses hosted tools
- Compatible with existing before_tool_call and after_tool_call hooks

Built with: Kimi K2.5 (Ollama Cloud) • OpenClaw v2026.2.4

src/agents/pi-tool-definition-adapter.ts
@@ -9,6 +9,7 @@ import { logDebug, logError } from "../logger.js";
 import { getGlobalHookRunner } from "../plugins/hook-runner-global.js";
 import { isPlainObject } from "../utils.js";
 import { runBeforeToolCallHook } from "./pi-tools.before-tool-call.js";
+import { runBeforeToolResultHook } from "./pi-tools.before-tool-result.js";
 import { normalizeToolName } from "./tool-policy.js";
 import { jsonResult } from "./tools/common.js";
 
@@ -79,7 +80,10 @@ function splitToolExecuteArgs(args: ToolExecuteArgsAny): {
   };
 }
 
-export function toToolDefinitions(tools: AnyAgentTool[]): ToolDefinition[] {
+export function toToolDefinitions(
+  tools: AnyAgentTool[],
+  hookContext?: { agentId?: string; sessionKey?: string },
+): ToolDefinition[] {
   return tools.map((tool) => {
     const name = tool.name || "tool";
     const normalizedName = normalizeToolName(name);
@@ -90,6 +94,7 @@ export function toToolDefinitions(tools: AnyAgentTool[]): ToolDefinition[] {
       parameters: tool.parameters,
       execute: async (...args: ToolExecuteArgs): Promise<AgentToolResult<unknown>> => {
         const { toolCallId, params, onUpdate, signal } = splitToolExecuteArgs(args);
+        const startMs = Date.now();
         try {
           // Call before_tool_call hook
           const hookOutcome = await runBeforeToolCallHook({
@@ -102,6 +107,26 @@ export function toToolDefinitions(tools: AnyAgentTool[]): ToolDefinition[] {
           }
           const adjustedParams = hookOutcome.params;
           const result = await tool.execute(toolCallId, adjustedParams, signal, onUpdate);
+          const durationMs = Date.now() - startMs;
+
+          // Run before_tool_result hook to allow plugins to modify or block the result
+          const outcome = await runBeforeToolResultHook({
+            toolName: name,
+            params: adjustedParams,
+            toolCallId,
+            result,
+            isError: false,
+            durationMs,
+            ctx: hookContext,
+          });
+
+          if (outcome.blocked) {
+            return jsonResult({
+              status: "blocked",
+              tool: normalizedName,
+              error: outcome.reason,
+            });
+          }
 
           // Call after_tool_call hook
           const hookRunner = getGlobalHookRunner();
@@ -111,7 +136,7 @@ export function toToolDefinitions(tools: AnyAgentTool[]): ToolDefinition[] {
                 {
                   toolName: name,
                   params: isPlainObject(adjustedParams) ? adjustedParams : {},
-                  result,
+                  result: outcome.result,
                 },
                 { toolName: name },
               );
@@ -122,8 +147,9 @@ export function toToolDefinitions(tools: AnyAgentTool[]): ToolDefinition[] {
             }
           }
 
-          return result;
+          return outcome.result;
         } catch (err) {
+          const durationMs = Date.now() - startMs;
           if (signal?.aborted) {
             throw err;
           }
@@ -165,7 +191,31 @@ export function toToolDefinitions(tools: AnyAgentTool[]): ToolDefinition[] {
             }
           }
 
-          return errorResult;
+          // Run before_tool_result hook for error results too
+          try {
+            const outcome = await runBeforeToolResultHook({
+              toolName: name,
+              params,
+              toolCallId,
+              result: errorResult,
+              isError: true,
+              durationMs,
+              ctx: hookContext,
+            });
+
+            if (outcome.blocked) {
+              return jsonResult({
+                status: "blocked",
+                tool: normalizedName,
+                error: outcome.reason,
+              });
+            }
+
+            return outcome.result;
+          } catch {
+            // If hook fails, fall back to original error result
+            return errorResult;
+          }
         }
       },
     } satisfies ToolDefinition;

ambushalgorithm/openclaw

src/agents/pi-tool-definition-adapter.ts
@@ -14,6 +14,7 @@ import {
   isToolWrappedWithBeforeToolCallHook,
   runBeforeToolCallHook,
 } from "./pi-tools.before-tool-call.js";
+import { runBeforeToolResultHook } from "./pi-tools.before-tool-result.js";
 import { normalizeToolName } from "./tool-policy.js";
 import { jsonResult } from "./tools/common.js";
 
@@ -62,56 +63,6 @@ function describeToolExecutionError(err: unknown): {
   return { message: String(err) };
 }
 
-function stringifyToolPayload(payload: unknown): string {
-  if (typeof payload === "string") {
-    return payload;
-  }
-  try {
-    const encoded = JSON.stringify(payload, null, 2);
-    if (typeof encoded === "string") {
-      return encoded;
-    }
-  } catch {
-    // Fall through to String(payload) for non-serializable values.
-  }
-  return String(payload);
-}
-
-function normalizeToolExecutionResult(params: {
-  toolName: string;
-  result: unknown;
-}): AgentToolResult<unknown> {
-  const { toolName, result } = params;
-  if (result && typeof result === "object") {
-    const record = result as Record<string, unknown>;
-    if (Array.isArray(record.content)) {
-      return result as AgentToolResult<unknown>;
-    }
-    logDebug(`tools: ${toolName} returned non-standard result (missing content[]); coercing`);
-    const details = "details" in record ? record.details : record;
-    const safeDetails = details ?? { status: "ok", tool: toolName };
-    return {
-      content: [
-        {
-          type: "text",
-          text: stringifyToolPayload(safeDetails),
-        },
-      ],
-      details: safeDetails,
-    };
-  }
-  const safeDetails = result ?? { status: "ok", tool: toolName };
-  return {
-    content: [
-      {
-        type: "text",
-        text: stringifyToolPayload(safeDetails),
-      },
-    ],
-    details: safeDetails,
-  };
-}
-
 function splitToolExecuteArgs(args: ToolExecuteArgsAny): {
   toolCallId: string;
   params: unknown;
@@ -136,7 +87,10 @@ function splitToolExecuteArgs(args: ToolExecuteArgsAny): {
   };
 }
 
-export function toToolDefinitions(tools: AnyAgentTool[]): ToolDefinition[] {
+export function toToolDefinitions(
+  tools: AnyAgentTool[],
+  hookContext?: { agentId?: string; sessionKey?: string },
+): ToolDefinition[] {
   return tools.map((tool) => {
     const name = tool.name || "tool";
     const normalizedName = normalizeToolName(name);
@@ -148,6 +102,7 @@ export function toToolDefinitions(tools: AnyAgentTool[]): ToolDefinition[] {
       parameters: tool.parameters,
       execute: async (...args: ToolExecuteArgs): Promise<AgentToolResult<unknown>> => {
         const { toolCallId, params, onUpdate, signal } = splitToolExecuteArgs(args);
+        const startMs = Date.now();
         let executeParams = params;
         try {
           if (!beforeHookWrapped) {
@@ -161,14 +116,30 @@ export function toToolDefinitions(tools: AnyAgentTool[]): ToolDefinition[] {
             }
             executeParams = hookOutcome.params;
           }
-          const rawResult = await tool.execute(toolCallId, executeParams, signal, onUpdate);
-          const result = normalizeToolExecutionResult({
-            toolName: normalizedName,
-            result: rawResult,
-          });
+          const result = await tool.execute(toolCallId, executeParams, signal, onUpdate);
+          const durationMs = Date.now() - startMs;
           const afterParams = beforeHookWrapped
             ? (consumeAdjustedParamsForToolCall(toolCallId) ?? executeParams)
             : executeParams;
+
+          // Run before_tool_result hook to allow plugins to modify or block the result
+          const outcome = await runBeforeToolResultHook({
+            toolName: name,
+            params: afterParams,
+            toolCallId,
+            result,
+            isError: false,
+            durationMs,
+            ctx: hookContext,
+          });
+
+          if (outcome.blocked) {
+            return jsonResult({
+              status: "blocked",
+              tool: normalizedName,
+              error: outcome.reason,
+            });
+          }
 
           // Call after_tool_call hook
           const hookRunner = getGlobalHookRunner();
@@ -178,7 +149,7 @@ export function toToolDefinitions(tools: AnyAgentTool[]): ToolDefinition[] {
                 {
                   toolName: name,
                   params: isPlainObject(afterParams) ? afterParams : {},
-                  result,
+                  result: outcome.result,
                 },
                 { toolName: name },
               );
@@ -189,8 +160,9 @@ export function toToolDefinitions(tools: AnyAgentTool[]): ToolDefinition[] {
             }
           }
 
-          return result;
+          return outcome.result;
         } catch (err) {
+          const durationMs = Date.now() - startMs;
           if (signal?.aborted) {
             throw err;
           }
@@ -235,7 +207,31 @@ export function toToolDefinitions(tools: AnyAgentTool[]): ToolDefinition[] {
             }
           }
 
-          return errorResult;
+          // Run before_tool_result hook for error results too
+          try {
+            const outcome = await runBeforeToolResultHook({
+              toolName: name,
+              params,
+              toolCallId,
+              result: errorResult,
+              isError: true,
+              durationMs,
+              ctx: hookContext,
+            });
+
+            if (outcome.blocked) {
+              return jsonResult({
+                status: "blocked",
+                tool: normalizedName,
+                error: outcome.reason,
+              });
+            }
+
+            return outcome.result;
+          } catch {
+            // If hook fails, fall back to original error result
+            return errorResult;
+          }
         }
       },
     } satisfies ToolDefinition;